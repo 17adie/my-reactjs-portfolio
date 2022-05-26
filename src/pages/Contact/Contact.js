c8e99dadd763c5ec7f170514f305475f31e44b4f
17adie <adief17@gmail.com>
2022-05-27 00:20:05 +0800

    wip contact page

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,40 @@
-import styles from './Contact.module.css'
+import styles from "./Contact.module.css"
+import ContactImg from "../../img/contact.svg"
 
-import Wip from '../../components/Wip/Wip'
+import { useForm } from "react-hook-form"
+
+// import Wip from '../../components/Wip/Wip'
 
 export default function Contact() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => console.log(data)
+  console.log(errors)
+
   return (
-    <Wip page="CONTACT" />
+    <div className={styles.container}>
+      <div className={styles["l-side"]}>
+        <img src={ContactImg} alt="Contact" />
+      </div>
+      <div className={styles["r-side"]}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label>Name</label>
+          <input type="text" placeholder="Name" {...register("Name", { required: true })} />
+          <label>Subject</label>
+          <input type="text" placeholder="Subject" {...register("Subject", { required: true })} />
+          <label>Email</label>
+          <input type="email" placeholder="Email" {...register("Email", { required: true, pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i })} />
+          <label>Message</label>
+          <textarea placeholder="Message" {...register("Message", { required: true })} />
+
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
+    // <Wip page="CONTACT" />
   )
 }
