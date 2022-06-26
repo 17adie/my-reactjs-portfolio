ea1093a765b09e84df019a4f8d4ae162ed0783be
17adie <adief17@gmail.com>
2022-06-26 23:42:23 +0800

    add loader when click submit button

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,6 +1,7 @@
 import styles from "./Contact.module.css"
 import ContactImg from "../../img/contact.svg"
 
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 
 import emailjs from "@emailjs/browser"
@@ -8,6 +9,9 @@ import emailjs from "@emailjs/browser"
 // import Wip from '../../components/Wip/Wip'
 
 export default function Contact() {
+  const [isSending, setisSending] = useState(false)
+  const [isSent, setisSent] = useState(false)
+
   const {
     register,
     handleSubmit,
@@ -16,16 +20,24 @@ export default function Contact() {
   } = useForm()
 
   const sendEmail = (formData) => {
-    console.log(formData)
+    setisSending(true)
+    setisSent(false)
+
     emailjs.send("service_icl1hlr", "template_m41raek", formData, "user_eNarrmAP5MHSpgC348mSW").then(
       (result) => {
         console.log(result.text)
+        setisSending(false)
+        setisSent(true)
+        reset()
       },
       (error) => {
         console.log(error.text)
       }
     )
-    reset()
+  }
+
+  const removeSentMessage = () => {
+    setisSent(false)
   }
 
   return (
@@ -36,14 +48,15 @@ export default function Contact() {
       <div className={styles["r-side"]}>
         <form onSubmit={handleSubmit(sendEmail)}>
           <label>Name</label>
-          <input type="text" placeholder="Name" {...register("user_name", { required: true })} />
+          <input type="text" placeholder="Name" onClick={removeSentMessage}{...register("user_name", { required: true })} />
           <label>Subject</label>
           <input type="text" placeholder="Subject" {...register("user_subject", { required: true })} />
           <label>Email</label>
           <input type="email" placeholder="Email" {...register("user_email", { required: true, pattern: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i })} />
           <label>Message</label>
           <textarea placeholder="Message" {...register("message", { required: true })} />
-
+          {isSending && <div className={styles.loader}></div>}
+          {isSent &&  <label className={styles.sent}>Message Sent!</label>}
           <input type="submit" />
         </form>
       </div>
