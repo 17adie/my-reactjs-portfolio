d61172accf46d1f22c5610f3dea2fbb9bc0146bd
17adie <adief17@gmail.com>
2022-07-10 23:46:09 +0800

    contact me and fixes

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -4,13 +4,39 @@ import ContactImg from "../../img/contact.svg"
 // import Wip from '../../components/Wip/Wip'
 import ContactForm from "../../components/ContactForm/ContactForm"
 
+// icons
+import { FaPhoneSquareAlt, FaEnvelopeSquare, FaHome } from "react-icons/fa"
+
 export default function Contact() {
+  const info = {
+    mobile: "0909 776 2803",
+    email: "aldrinefacistol@yahoo.com",
+    address: "Calamba City, Laguna 4027",
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles["l-side"]}>
         <img src={ContactImg} alt="Contact" />
       </div>
       <div className={styles["r-side"]}>
+        <div className={styles["contact-me"]}>
+          <h2>Contact me</h2>
+          <div className={styles["contact-info"]}>
+            <div>
+              <FaPhoneSquareAlt />
+              <span>{info.mobile}</span>
+            </div>
+            <div>
+              <FaEnvelopeSquare />
+              <span>{info.email}</span>
+            </div>
+            <div>
+              <FaHome />
+              <span>{info.address}</span>
+            </div>
+          </div>
+        </div>
         <ContactForm />
       </div>
     </div>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,38 +1,40 @@
-import styles from './Home.module.css'
+import styles from "./Home.module.css"
 
 // icons
-import { FaGithub, FaFacebook, FaLinkedin, FaFilePdf  } from 'react-icons/fa';
+import { FaGithub, FaFacebook, FaLinkedin } from "react-icons/fa"
 
 // svg
-import ProgrammingSvg from '../../img/programming-coffee.svg';
+import ProgrammingSvg from "../../img/programming-coffee.svg"
 
 export default function Home() {
-
   const links = {
-    github: 'https://github.com/17adie',
-    facebook: 'https://www.facebook.com/17aldrn/',
-    linkedin: 'https://www.linkedin.com/in/aldrinefacistol/'
+    github: "https://github.com/17adie",
+    facebook: "https://www.facebook.com/17aldrn/",
+    linkedin: "https://www.linkedin.com/in/aldrinefacistol/",
   }
 
   return (
     <div className={styles.container}>
-      <div className={styles['l-side']}>
+      <div className={styles["l-side"]}>
         <div className={styles.info}>
-
-          <span>Hi There!</span>
+          <span>Hi there!</span>
           <span>ALDRINE FACISTOL</span>
           <span>Web Developer</span>
-
         </div>
 
         <div className={styles.icons}>
-            <a href={links.github} target="_blank"><FaGithub /></a>
-            <a href={links.linkedin} target="_blank"><FaLinkedin /></a>
-            <a href={links.facebook} target="_blank"><FaFacebook /></a>
+          <a href={links.github} target="_blank">
+            <FaGithub />
+          </a>
+          <a href={links.linkedin} target="_blank">
+            <FaLinkedin />
+          </a>
+          <a href={links.facebook} target="_blank">
+            <FaFacebook />
+          </a>
         </div>
-       
       </div>
-      <div className={styles['r-side']}>
+      <div className={styles["r-side"]}>
         <img src={ProgrammingSvg} alt="Programming" />
       </div>
     </div>
